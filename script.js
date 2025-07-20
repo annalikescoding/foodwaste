673014c9d9319650f63dea0bc9d31b27da202bb6
annalikescoding <annalac2139@gmail.com>
2025-07-20 13:28:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* .js files add interaction to your website */
 var reducewaylist = [
-  "Sort your food waste into the compost bin!",
   "Compost at home! You can use food waste to create nutrient-rich soil.",
   "Buy the amount of food you need, not want.",
   "Store food properly to prevent spoilage.",
   "Plan meals for the week and only buy the ingredients you need.",
   "Do not have a clustered fridge! If you do use up the food in your fridge before it spoils.",
+  "Sort your food waste into the compost bin!",
 ];
 
 console.log(reducewaylist);
